b59f625ee31400657f37b04b844c184bc52a70b2
rishithaw <rishithaw1@gmail.com>
2025-12-02 20:00:19 -0600

    Fixed Styling

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,3 @@
 //= link_tree ../images
 //= link_tree ../builds
 //= link_directory ../stylesheets .css
-//= link_directory ../../application .js
-
-//= link application.css
-//= link application.js
-//= link active_admin.css
-//= link active_admin.js
--- app/assets/javascripts/application.js
+++
@@ -1,4 +0,0 @@
-//= require rails-ujs
-//= require active_admin
-import "bootstrap"
-import "bootstrap/dist/js/bootstrap.bundle"
